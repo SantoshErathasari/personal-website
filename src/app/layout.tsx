b0b3fe3cb388bb4c0fb7ef88850b6ff2e2257ce4
Santosh <serathasari.escape@gmail.com>
2024-02-25 16:27:51 -0500

    Added animations to text and changed color theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className='bg-slate-900 text-slate-100'>
+    <html lang="en" className='bg-green-950 text-green-500'>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,15 +1,64 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import gsap from 'gsap';
 
 /**
  * Props for `Hero`.
  */
+
+//47:43
 export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
 /**
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+  const component = useRef(null)
+
+   useEffect(()=>{
+     let ctx = gsap.context(()=>{
+
+      const tl = gsap.timeline()
+      tl.fromTo(".name-animation",{
+        x: -100, opacity: 0, rotate: -10
+      },
+      {
+        x: 0,
+        opacity: 1,
+        rotate: 0,
+        ease: "bounce.out",
+        duration: 1,
+        transformformOrigin: 'left top',
+        delay: 0.5,
+        stagger: {
+          each: 0.07,
+          from: 'random',
+        },
+
+      }
+      );
+
+      tl.fromTo(".job-title", {
+        y:20,
+        opacity:0,
+        scale:1.2,
+      }, {
+        opacity: 1,
+        y:0,
+        duration: 1,
+        scale:1,
+        ease: "bounce.out",
+
+
+      })
+
+
+     }, component)
+     return () => ctx.revert();
+     }, []);
 
   //function that makes every letter with individual span
 
@@ -27,16 +76,19 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      ref={component}
     >
       <div className= 'grid min-height-[70vh] grid-cols-1 md:grid-cols-2 items-center'>
         <div className ='col-start-1 md:row-start-1'>
 
           <h1 className ='mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter' aria-label={slice.primary.santosh + '' + slice.primary.erathasari}>
-          <span className ='block text-slate-300'>{renderLetters(slice.primary.santosh ,'first')}</span>
-          <span className ='-mt-[.2em] block text-slate-500'>{renderLetters(slice.primary.erathasari, 'first')}</span>
+          <span className ='block text-green-300'>{renderLetters(slice.primary.santosh ,'first')}</span>
+          <span className ='-mt-[.2em] block text-green-500'>{renderLetters(slice.primary.erathasari, 'first')}</span>
           </h1>
 
-          <span className='block bg-gradient-to-tr from-yellow-500 via-red-200 to-yellow-500 bg-clip-text text-2x1 font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4x1'>{slice.primary.dedicated_student}</span>
+          <span className='job-title block bg-gradient-to-tr from-green-500 via-lime-200 to-green-500 bg-clip-text text-2x1 font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4x1'>
+            {slice.primary.dedicated_student}
+            </span>
 
         </div>
       </div>

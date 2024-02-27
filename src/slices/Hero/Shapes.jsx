80ad4f0750a66fc2f396e5f2ffe8780846b8c4e1
Santosh <serathasari.escape@gmail.com>
2024-02-26 21:35:22 -0500

    fixed clicking issue with 3d object

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -1,97 +1,99 @@
-'use client';
+"use client";
 
-import * as Three from 'three';
-import {Canvas } from "@react-three/fiber";
-import { ContactShadows, Float, Environment } from '@react-three/drei';
-import { Suspense, useEffect, useRef, useState } from 'react';
-import { gsap } from 'gsap';
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { ContactShadows, Float, Environment } from "@react-three/drei";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
 
-export default function Shapes(){
-    return(
-        <div className ='row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0'>
-            <Canvas className='z-0' shadows gl={{antialias: false}} dpr={[1, 1.5]} camera={{position: [0, 0, 25], fov: 30, near:1, far:40}}>
-                <Suspense fallback={null}>
-                    <Geometries/>
-                    <ContactShadows position={[0, -3.5, 0]} opacity={0.65} scale={40} blur={1} far={9}>
-                        <Environment preset='studio' />
-
-                        
-                    </ContactShadows>
-
-                </Suspense>
-            </Canvas>
-        </div>
-    );
+export default function Shapes() {
+  return (
+    <div className="row-span-1 row-start-1 -mt-9 aspect-square  md:col-span-1 md:col-start-2 md:mt-0">
+      <Canvas className="z-0" shadows gl={{ antialias: false }} dpr={[1, 1.5]} camera={{ position: [0, 0, 25], fov: 30, near: 1, far: 40 }}>
+        <Suspense fallback={null}>
+          <Geometries />
+          <ContactShadows position={[0, -3.5, 0]} opacity={0.65} scale={40} blur={1} far={9}/>
+          <Environment preset="studio" />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
 
-function Geometries(){
-    const geometries = [
-        {
-            position: [0, 0, 0],
-            r: 0.3, 
-            geometry: new Three.IcosahedronGeometry(3), //topright
+function Geometries() {
+  const geometries = [
+    {
+      position: [0, 0, 0],
+      r: 0.3,
+      geometry: new THREE.IcosahedronGeometry(3), // Gem
+    },
+  ];
 
-        },
-    ];
 
-    const materials = [new Three.MeshNormalMaterial()];
+  const materials = [
+    new THREE.MeshNormalMaterial(),
+  ];
 
-    return geometries.map(({position, r, geometry})=>(
-        <Geometry key={JSON.stringify(position)} position={position.map((p)=>p*2)} geometry ={geometry} materials ={materials} r={r}></Geometry>
-    ));
-
+  return geometries.map(({ position, r, geometry }) => (
+    <Geometry key={JSON.stringify(position)} position={position.map((p) => p * 2)} geometry={geometry} materials={materials} r={r}
+    />
+  ));
 }
 
-function Geometry({r, position, geometry, materials}){
-    const meshRef = useRef();
-    const [visible, setVisible] = useState(false);
-    const startingMaterial = getRandomMaterial();
+function Geometry({ r, position, geometry, materials }) {
+  const meshRef = useRef();
+  const [visible, setVisible] = useState(false);
 
-        function getRandomMaterial(){
-            return gsap.utils.random(materials);
-        }
+  const startingMaterial = getRandomMaterial();
 
-        function handleClick(e){
-            const mesh = e.object;
+  function getRandomMaterial() {
+    return gsap.utils.random(materials);
+  }
 
-            gsap.to(mesh.rotation,{
-                x: '+=${gsap.utils.random(0,2)}',
-                y: '+=${gsap.utils.random(0,2)}',
-                z: '+=${gsap.utils.random(0,2)}',
-                duration: 1.3,
-                ease: 'elastic.out(1,0.3)',
-                yoyo: true,
-            });
-            mesh.material = getRandomMaterial();
-        }
+  function handleClick(e) {
+    const mesh = e.object;
 
-        const handlePointerOver = () =>{
-            document.body.style.cursor ='pointer'
-        }
-        const handlePointerOut = () =>{
-            document.body.style.cursor ='default'
-        }
+    gsap.to(mesh.rotation, {
+      x: `+=${gsap.utils.random(0, 2)}`,
+      y: `+=${gsap.utils.random(0, 2)}`,
+      z: `+=${gsap.utils.random(0, 2)}`,
+      duration: 1.3,
+      ease: "elastic.out(1,0.3)",
+      yoyo: true,
+    });
 
-        useEffect(()=>{
-            gsap.context(()=>{
-                setVisible(true);
-                gsap.from(meshRef.current.scale,{
-                    x:0,
-                    y:0,
-                    z:0,
-                    duration: 1.3,
-                    ease: 'elastic.out(1,0.3)',
-                    delay: 0.3,
+    mesh.material = getRandomMaterial();
+  }
 
-                })
-            })
-        }, [])
+  const handlePointerOver = () => {
+    document.body.style.cursor = "pointer";
+  };
 
-        return(
-            <group position ={position} ref = {meshRef}>
-                <Float speed={5*r} rotationIntensity={6*r} floatIntensity={5*r}>
-                    <mesh geometry={geometry} onClick={handleClick} onPointerOver={handlePointerOver} onPointerOut={handlePointerOut} visible={visible} material={startingMaterial}/>
-                </Float>
-            </group>
-        )
+  const handlePointerOut = () => {
+    document.body.style.cursor = "default";
+  };
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      setVisible(true);
+      gsap.from(meshRef.current.scale, {
+        x: 0,
+        y: 0,
+        z: 0,
+        duration: gsap.utils.random(0.8, 1.2),
+        ease: "elastic.out(1,0.3)",
+        delay: gsap.utils.random(0, 0.5),
+      });
+    });
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <group position={position} ref={meshRef}>
+      <Float speed={5 * r} rotationIntensity={6 * r} floatIntensity={5 * r}>
+        <mesh geometry={geometry} onClick={handleClick} onPointerOver={handlePointerOver} onPointerOut={handlePointerOut} visible={visible} material={startingMaterial}
+        ></mesh>
+      </Float>
+    </group>
+  );
 }

2838718a59653f768afb72312991b58aab5d4ff6
Santosh <serathasari.escape@gmail.com>
2024-03-07 13:03:38 -0500

    updating object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Orbitron } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Orbitron({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className='bg-green-950 text-green-500'>
       <body className={inter.className}>{children}</body>
     </html>
   );
